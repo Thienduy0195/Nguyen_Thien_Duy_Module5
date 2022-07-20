781d4b5f93079073d861486361053a0335430563
Thienduy0195 <thienduy0195@gmail.com>
2022-07-20 07:17:35 +0700

    ss5

---
+++ _ss97_case_study_module5/angular-case-study/src/app/service-list/service.ts
@@ -0,0 +1,16 @@
+export interface Service {
+  id:number;
+  type:string;
+  name:string;
+  area: number;
+  cost:number;
+  maxPeople:number;
+  roomStandard:string;
+  otherConvenience:string;
+  poolArea:number;
+  floor:number;
+  freeService:string;
+  code:string;
+  rentType:string;
+  imageUrl:string;
+}
